db491b4866313ce2feb942cf5603cc701be868ce
komalharjani <54577325+komalharjani@users.noreply.github.com>
2019-11-27 01:04:05 +0000

    Create Circle.java

---
+++ src/model/Circle.java
@@ -0,0 +1,12 @@
+package model;
+
+import java.awt.*;
+
+public class Circle extends Shape {
+
+
+    public Circle(int firstX, int firstY, int lastX, int lastY, Color colour, boolean fill) {
+        super(firstX, firstY, lastX, lastY, colour, fill);
+    }
+
+}
